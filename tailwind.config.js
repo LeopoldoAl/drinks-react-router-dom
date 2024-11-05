88361af280a22c1ac0bc23c5cdff082cc4bef465
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-05 09:43:43 -0400

    Step 27

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "header": "url(/bg.jpg)"
+      }
+    },
   },
   plugins: [],
 }

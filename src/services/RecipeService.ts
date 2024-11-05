79dbc715a9917bbe5b140e59a0507569772f1add
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-05 12:24:43 -0400

    Step 41

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,5 +1,14 @@
-// www.thecocktaildb.com/api/json/v1/1/list.php?c=list
+import { CategoriesAPIResponseShema } from "../utils/recipes.schemas"
+import axios from "axios"
+
+
 
 export async function getCategories() {
-    console.log('From the RecipeService')
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+    const {data}  = await axios(url)
+    const result = CategoriesAPIResponseShema.safeParse(data)
+
+    if (result.success) {
+        return result.data
+    }
 }

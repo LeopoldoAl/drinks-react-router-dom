aca546cd6d0c0ece839323945d887f93f677a9d2
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-07 10:28:21 -0400

    Step 54

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
-import { CategoriesAPIResponseShema, DrinksAPIResponse, SearchFilterSchema } from "../utils/recipes.schemas"
+import { CategoriesAPIResponseShema, DrinkAPIResponse, DrinksAPIResponse, SearchFilterSchema } from "../utils/recipes.schemas"
 
 export type Categories = z.infer<typeof CategoriesAPIResponseShema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
+export type Drink = z.infer<typeof DrinkAPIResponse>

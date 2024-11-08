6176b67f822a71cde81981157ec6d20c8c427ffa
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-08 11:06:25 -0400

    Step 74

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { createRecipesSlice, RecipesSlicesType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { FavoritesSliceType, createFavoritesSlice } from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSlicesType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a)
+export const useAppStore = create<RecipesSlicesType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
 
 })))
